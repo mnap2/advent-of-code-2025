fe4ce828474c68e3153de2c26f510aeb68b10570
m nap <xyz66i@o2.pl>
2026-01-02 01:59:02 +0100

    add day3 part two solution

--- __tests__/Day3.test.ts
+++ __tests__/Day3.test.ts
@@ -1,6 +1,13 @@
-import { Lobby } from "../solutions/Day3";
+import { Lobby, Lobby2 } from "../solutions/Day3";
+import fs from "fs"
+
+let readFileSyncSpy = jest.spyOn(fs, 'readFileSync');
 
 describe("lobby tests", () => {
+    beforeEach(() => {
+        readFileSyncSpy.mockRestore()
+    })
+
     describe("part one", () => {
         it('should return actual solution to the puzzle', () => {
             expect(Lobby()).toBe(17193);
@@ -8,6 +15,26 @@ describe("lobby tests", () => {
     })
 
     describe("part two", () => {
+        it.each([
+            {
+                description: "should return correct answer when input is 234234234234278",
+                input: "234234234234278\n",
+                answer: 434234234278
+            },
+            {
+                description: "should return correct answer when input is taken from the example in the task description",
+                input: "987654321111111\n811111111111119\n234234234234278\n818181911112111\n",
+                answer: 3121910778619
+            }
+        ])('$description', ({input, answer}) => {
+            readFileSyncSpy = jest.spyOn(fs, 'readFileSync');
+            readFileSyncSpy.mockReturnValue(input)
 
+            expect(Lobby2()).toBe(answer);
+        });
+
+        it('should return actual solution to the puzzle', () => {
+            expect(Lobby2()).toBe(171297349921310);
+        });
     })
 });
--- solutions/Day3.ts
+++ solutions/Day3.ts
@@ -23,3 +23,38 @@ export const Lobby = () => {
 
     return totalMaxJoltage;
 };
+
+export const Lobby2 = () => {
+    const filePath = path.join(__dirname, "../inputs/Day3-input.txt");
+    const data = fs.readFileSync(filePath, "utf-8");
+    let totalMaxJoltage = 0;
+
+    for(const bank of data.split("\n")) {
+        const bankIntArray = bank.split("").map((value: string) => Number(value));
+
+        if(bankIntArray.length !== 0) {
+            let prevBatteryIndex = 0;
+            let maxBankJoltageStr = "";
+
+            for(let batteriesLeft = 12; batteriesLeft > 0; batteriesLeft--) {
+                const subarrayToSearchIn = bankIntArray.slice(
+                    prevBatteryIndex, 
+                    batteriesLeft !== 1
+                        ? - batteriesLeft + 1
+                        : undefined
+                )
+
+                const batteryVal = Math.max(...subarrayToSearchIn);
+
+                const batteryIndex = prevBatteryIndex + subarrayToSearchIn.indexOf(batteryVal) + 1;
+                prevBatteryIndex = batteryIndex;
+                
+                maxBankJoltageStr += batteryVal.toString();
+            }
+
+            totalMaxJoltage += Number(maxBankJoltageStr);
+        }
+    }
+
+    return totalMaxJoltage;
+};
